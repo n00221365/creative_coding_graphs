50d4769cd6e309a521327f4a0d905564ff5671e1
n00221365 <n00221365@iadt.ie>
2024-02-28 04:10:51 +0000

    Finished stacked horizontal chart, did entire age chart, small fixes

--- Normal Bar Chart/BarChart.js
+++ Normal Bar Chart/BarChart.js
@@ -322,27 +322,21 @@ class HorizontalBarChart{
     print( this.data);
         for(let i=0; i<this.data.length; i++){
             push();
-            fill(173, 216, 230);
+            fill(64,181,240);
             rect (0,0,this.barWidth, -this.data[i][this.bottomYValue]*scale);
             translate(0,-this.data[i][this.bottomYValue]*scale)
             fill(255,192,203)
             rect (0,0,this.barWidth, -this.data[i][this.bottomYValue,this.topYValue]*scale);
-            // console.log( -this.data[i][this.topYValue]*scale);
             pop();
-
             textSize(10)
-           
             noStroke();
             fill(255, 249, 165);
             textAlign(LEFT, CENTER);
-
             push();
             translate(this.barWidth/2,10);
             rotate (45)
             text(labels[i],0,0);
             pop()
-
-
             translate(gap+this.barWidth,0)
             
         }
@@ -368,15 +362,12 @@ console.log(totalMaxValue)
                     let tickNum =  totalMaxValue /5;
                     let tickValue = tickNum * i;
                     
-
                     stroke("#fff")
                     line(0,-i*tickGap,-20,-i*tickGap);
                     textSize(10)
-           
                     noStroke();
                     fill(255, 249, 165);
                     textAlign(RIGHT,BOTTOM);
-        
                     push();
                     translate(this.barWidth/2,10);
                     rotate (45)
@@ -385,30 +376,23 @@ console.log(totalMaxValue)
                     text(round(tickValue),0,-i*tickGap)
                     ;
                 }
-        pop ();
-
-        
+        pop ();  
     }
 }
 
+class HorStacked{
 
-class AgeChart{
-
-    
     constructor(obj){
         this.title = obj.title;
         this.yAxisTitle = obj.yAxisTitle;
         this.xAxisTitle = obj.xAxisTitle;
         this.total = obj.total;
         this.data = obj.data;
-
         this.bottomXValue = obj.bottomXValue;
         this.topXValue = obj.topXValue;
         this.yValue = obj.yValue;
-
         this.bottomValueTitle=obj.bottomValueTitle;
         this.topValueTitle=obj.topValueTitle;
-
         this.chartWidth=obj.chartWidth;
         this.chartHeight=obj.chartHeight;
         this.xPos = obj.xPos;
@@ -418,16 +402,12 @@ class AgeChart{
         this.labelColour = obj.labelColour;
         this.labelRotation = obj.labelRotation;
         this.barWidth = obj.barWidth;
-
- 
     }
 
     render(){
        
         push ();
         translate (this.xPos,this.yPos);
-
-    
 
          //BAR CHART TITLE
          textSize(30);
@@ -438,14 +418,14 @@ class AgeChart{
          //X AXIS TITLE
  
          textSize(20);
-         textAlign(CENTER)
+         textAlign(LEFT)
          fill(255);
          text(this.xAxisTitle, this.chartWidth/2,185)
          //Y AXIS TITLE
          push()
          textSize(20);
          fill(255);
-         textAlign(CENTER, CENTER)
+         textAlign(LEFT)
         translate (-100,-this.chartHeight/2)
         rotate (90);
          text(this.yAxisTitle,0,0);
@@ -456,20 +436,13 @@ class AgeChart{
         line (0,0,0,-this.chartHeight);
         line (0,0,this.chartWidth,0);
 
-    //    let totals = [];
-    //    for(let i=0; i<this.data.length; i++){
-    //   let  total = this.data[i][this.topYValue] + this.data[i][this.bottomYValue];
-    //    totals.push(total);
-    //    }
-    //    console.log()
-
        let topMaxValue = max(this.data.map(d => d[this.topXValue]))
        let bottomMaxValue = max(this.data.map(d => d[this.bottomXValue]))
        let totalMaxValue = (topMaxValue + bottomMaxValue)
         let gap = 20;
         let labels = this.data.map(d => d[this.yValue]);
         //let total = this.bottomYValue + this.topYValue;
-        let scale = this.chartHeight / totalMaxValue;
+        let scale = this.chartWidth / (max(this.data.map(d => d[this.bottomXValue])) + max(this.data.map(d => d[this.topXValue])));
 
         console.log(totalMaxValue);
       
@@ -479,42 +452,41 @@ class AgeChart{
         translate(0,-this.chartHeight);
 
         for(let i=0; i<this.data.length; i++){
-            fill(173, 216, 230);
-            rect (0,0,this.data[i][this.bottomXValue],this.barWidth);
+            fill(64,181,240);
+            rect (0,gap,this.data[i][this.bottomXValue]*scale,this.barWidth);
 
             push()
-            translate(this.data[i][this.bottomXValue],0)
+            translate(this.data[i][this.bottomXValue]*scale,0)
             fill(255,192,203)
-            rect (0,0, this.data[i][this.topXValue],this.barWidth);
+            rect (0,gap, this.data[i][this.topXValue]*scale,this.barWidth);
             pop()
 
+            push();
+            translate(0,-gap);
             textSize(10)
             noStroke();
             fill(255, 249, 165);
-            textAlign(LEFT, CENTER);
-            push();
-            translate(0,-this.barHeight);
+            textAlign(LEFT);
             text(labels[i],0,gap+this.barWidth);
             pop()
 
-
-            translate(gap+this.barWidth,0)
+            translate(0,gap+this.barWidth)
             
         }
 
         let sumTop = 0;
         let sumBottom= 0;
         for(let i=0; i<this.data.length;i++){
-            sumTop += int(this.data[i].VALUE2019);
-            sumBottom += int(this.data[i].VALUE2018);
+            sumTop += int(this.data[i].MALEVALUE);
+            sumBottom += int(this.data[i].FEMALEVALUE);
         };
         let sumTotal= sumTop + sumBottom;
         console.log(sumTotal);
 
 
-        let averageLine = sumTotal / this.data.length;
+        let averageLine = sumTotal ;
         stroke(255,0,0);
-        line(0,-averageLine,-this.chartWidth,-averageLine)
+        line(averageLine,-this.chartHeight+90,averageLine,0)
         pop()
                 // THis draws the vertical elements
             let tickGap = this.chartHeight/5;
@@ -526,7 +498,7 @@ class AgeChart{
            
                     noStroke();
                     fill(255, 249, 165);
-                    textAlign(RIGHT,BOTTOM);
+                    textAlign(LEFT,BOTTOM);
         
                     push();
                     translate(this.barWidth/2,10);
@@ -536,13 +508,11 @@ class AgeChart{
                     text(round(tickValue*i),i*tickGap,40)
                     ;
                 }
-        pop ();
-
-        
+        pop (); 
     }
 }
 
-class Pictograph{
+class AgeChartRight{
     constructor(obj){
         this.title = obj.title;
         this.yAxisTitle = obj.yAxisTitle;
@@ -564,7 +534,6 @@ class Pictograph{
     }
 
     render(){
-       
         push ();
         translate (this.xPos,this.yPos);
 
@@ -573,14 +542,14 @@ class Pictograph{
          fill(255);
         noStroke()
         textAlign(CENTER);
-         text(this.title, this.chartWidth / 2,-350);
+         text(this.title, this.chartWidth / 15,-250);
  
          //X AXIS TITLE
  
          textSize(20);
          textAlign(CENTER)
          fill(255);
-         text(this.xAxisTitle, this.chartWidth/2,185)
+         text(this.xAxisTitle, 50,100)
          //Y AXIS TITLE
          push()
          textSize(20);
@@ -588,7 +557,7 @@ class Pictograph{
          textAlign(CENTER, CENTER)
         translate (-100,-this.chartHeight/2)
         rotate (90);
-         text(this.yAxisTitle,0,0);
+         text(this.yAxisTitle,0,this.chartWidth,0);
          
         pop();
  
@@ -605,8 +574,8 @@ class Pictograph{
          push()
          translate(0,-this.chartHeight);
          for(let i=0; i<this.data.length; i++){
-             fill(91, 192, 190);
-             rect (0,i * gap, this.data[i][this.xValue]*scale,this.barWidth);
+             fill(255,0,0);
+             rect (0,i * gap-20, this.data[i][this.xValue]*scale,this.barWidth);
  
  
              textSize(10)
@@ -646,6 +615,112 @@ class Pictograph{
                      rotate (45)
                      pop()
                      text(round(tickValue*i),i*tickGap,40)
+                     ;
+
+                 }
+ 
+         pop ();
+     }
+ }
+
+ class AgeChartLeft{
+    constructor(obj){
+        this.title = obj.title;
+        this.yAxisTitle = obj.yAxisTitle;
+        this.xAxisTitle = obj.xAxisTitle;
+
+        this.data = obj.data;
+        this.yValue = obj.yValue;
+        this.xValue = obj.xValue;
+
+        this.chartWidth=obj.chartWidth;
+        this.chartHeight=obj.chartHeight;
+        this.xPos = obj.xPos;
+        this.yPos = obj.yPos;
+        this.axisLineColour = obj.axisLineColour;
+        this.labelPadding = obj.labelPadding;
+        this.labelColour = obj.labelColour;
+        this.labelRotation = obj.labelRotation;
+        this.barWidth = obj.barWidth;
+    }
+
+    render(){
+       
+        push ();
+        translate (this.xPos,this.yPos);
+
+         //BAR CHART TITLE
+         textSize(30);
+         fill(255);
+        noStroke()
+        textAlign(CENTER);
+ 
+         //X AXIS TITLE
+ 
+         textSize(20);
+         textAlign(CENTER)
+         fill(255);
+         text(this.xAxisTitle, this.chartWidth-this.chartWidth -30,100)
+
+         //Y AXIS TITLE
+         push()
+         textSize(20);
+         fill(255);
+         textAlign(CENTER, CENTER)
+        translate (-100,-this.chartHeight/2)
+        rotate (90);
+         
+        pop();
+ 
+        stroke(this.axisLineColour)
+        line (0,0,0,-this.chartHeight);
+        line (0,0,this.chartWidth,0);
+
+        let gap = (this.chartHeight - (this.data.length))/(this.data.length +28);
+        let labels = this.data.map(d => d[this.yValue]);
+        let scale = this.chartWidth / max(this.data.map(d => d[this.xValue]));
+      
+
+         // THis draws the horizontal elements
+         push()
+         translate(0,-this.chartHeight);
+         for(let i=0; i<this.data.length; i++){
+             fill(0,0,255);
+             rect (0,i * gap -20, -this.data[i][this.xValue]*scale,this.barWidth);
+ 
+ 
+             textSize(10)
+            
+             noStroke();
+             fill(255, 249, 165);
+             textAlign( RIGHT, CENTER);
+ 
+ 
+             translate(0,gap+this.barWidth)
+             
+         }
+         
+         pop()
+                 // THis draws the vertical elements
+             let tickGap = this.chartWidth/5;
+             let tickValue =  max(this.data.map(d => d[this.xValue])) *5
+ 
+             
+                 for(let i=0; i<=5; i++){
+                     stroke("#fff")
+                     line(i*-tickGap,5,i*-tickGap,20);
+                     textSize(10)
+            
+                     noStroke();
+                     fill(255, 249, 165);
+                     textAlign(CENTER);
+                     
+         
+                     push();
+                     translate(this.barWidth/2,10);
+                     rotate (45)
+                     pop()
+                     text(round(tickValue*-i),-i*tickGap,40)
                      ;
 
                  }
--- Normal Bar Chart/sketch.js
+++ Normal Bar Chart/sketch.js
@@ -1,27 +1,40 @@
+
 let barCharts = [];
 let data1;
 let data2;
 let data3;
 let data4;
 let data5;
+let data6;
+
 
 let cleanData = [];
 let cleanData2 = [];
 let cleanData3 = [];
 let cleanData4 = [];
 let cleanData5 = [];
+let cleanData6 = [];
+
 
 let numRows;
 let numRows2;
 let numRows3;
 let numRows4;
 let numRows5;
+let numRows6;
+
 
 function preload() {
-  data1 = loadTable("data/property_price1.csv", "csv", "header");
-  data2 = loadTable("data/test5.csv", "csv", "header");
-  data3 = loadTable("data/property_price.csv", "csv", "header");
-  data4 = loadTable("data/test2.csv", "csv", "header");
+
+
+  data1 = loadTable("data/normalChart.csv", "csv", "header");
+  data2 = loadTable("data/horizontalChart.csv", "csv", "header");
+  data3 = loadTable("data/stackedChart.csv", "csv", "header");
+  data4 = loadTable("data/horStackedChart.csv", "csv", "header");
+  data5 = loadTable("data/ageGroupChart.csv", "csv", "header");  
+  data6 = loadTable("data/ageGroupChart.csv", "csv", "header");
+
+
 }
 
 function setup() {
@@ -140,7 +153,7 @@ let barChart4 = {
   chartWidth: 700,
   chartHeight: 300,
 
-  xPos: 1050,
+  xPos: 1150,
   yPos: 1150,
 
   axisLineColour: "#d9d9d9",
@@ -150,11 +163,58 @@ let barChart4 = {
   labelColour: "#db1f83",
   labelRotation: 0,
 };
-barCharts.push(new AgeChart(barChart4));
+barCharts.push(new HorStacked(barChart4));
+
+
+numRows5 = data5.rows.length;
+  for (let i = 0; i < numRows5; i++) {
+    cleanData5.push(data5.rows[i].obj);
+  }
+
+let barChart05 = {
+  data: cleanData5,
+  title: "Comparison of single living individuals by age",
+  xAxisTitle: "Women",
+  yAxisTitle: "Age",
+  xValue: "MALEVALUE",
+  yValue: "Age Group",
+  chartWidth: 700,
+  chartHeight: 180,
+  xPos: 1150,
+  yPos: 1800,
+  axisLineColour: "#d9d9d9",
+  barWidth: 20,
+  labelTextSize: 130,
+  labelPadding: 10,
+  labelColour: "#db1f83",
+  labelRotation: 0,
+};
+barCharts.push(new AgeChartRight(barChart05));
+
+
+numRows6 = data6.rows.length;
+  for (let i = 0; i < numRows6; i++) {
+    cleanData6.push(data6.rows[i].obj);
+  }
+
+let barChart06 = {
+  data: cleanData6,
+  xAxisTitle: "Men",
+  xValue: "FEMALEVALUE",
+  yValue: "Age Group",
+  chartWidth: 700,
+  chartHeight: 180,
+  xPos: 1085,
+  yPos: 1800,
+  axisLineColour: "#d9d9d9",
+  barWidth: 20,
+  labelTextSize: 130,
+  labelPadding: 10,
+  labelColour: "#db1f83",
+  labelRotation: 0,
+};
+barCharts.push(new AgeChartLeft(barChart06));
 }
-
-
-
 function draw() {
   background(11, 19, 43);
   barCharts.forEach(bar => bar.render());
